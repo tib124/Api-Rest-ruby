9b2f4ab3cafc02f92347dc1bfaba56328adfdf69
Thiago Oliveira <thiago.augusto@ima.sp.gov.br>
2025-05-08 13:45:15 -0300

    Added JWT Token AUthentication to api calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate
+
+    rescue_from JWT::VerificationError, with: :invalid_token
+    rescue_from JWT::DecodeError, with: :decode_error
+
+
+
+    private
+    def authenticate
+        authorization_header = request.headers['Authorization']
+        token = authorization_header.split(' ').last if authorization_header
+        decoded_token = JsonWebToken.decode(token)
+
+       @user = User.find(decoded_token[:user_id])
+    end
+
+    def invalid_token
+        render json: { message: 'Invalid token' }, status: :unauthorized
+    end
+    def decode_error
+        render json: { message: 'Token decode error' }, status: :unauthorized
+    end
 end
